b72268e6fb4b413c2870f5f0c0149bd19c872484
shu <free1139@163.com>
2018-07-14 17:35:03 +0800

    fix sup_etc

--- env.sh
+++ env.sh
@@ -10,29 +10,27 @@ export GOLIB="$(dirname "$PRJ_ROOT")/golib" # 作为公共库使用
 export GOPATH=$GOLIB:$PRJ_ROOT
 export GOBIN=$PRJ_ROOT/bin
 
-# 设定sup [command] all 的遍历目录
-export build_all="$PRJ_ROOT/src/service/app $PRJ_ROOT/src/applet/web"
+# 设定sup [command] all 所遍历的目录
+export BUILD_ALL_PATH="$PRJ_ROOT/src/service/app $PRJ_ROOT/src/applet/web"
 
-# 以下配置可使用默认配置即可 
+# supervisord配置文件参数
 # --------------------START-------------------
-# 设定sup部署使用的环境
 # 以下是部署时的supervisor默认配置数据，若未配置时，会使用以下默认数据
 # 开发IDE可不配置以下环境变量
 # 配置supervisor的配置文件目录
 # export SUP_ETC_DIR="/etc/supervisor/conf.d/" # (可选)
 # 配置supervisor的子程序日志的单个文件最大大小
-export SUP_LOG_SIZE="10MB"
+# export SUP_LOG_SIZE="10MB"
 # 配置supervisor的子程序日志的最多文件个数
-export SUP_LOG_BAK="10"
+# export SUP_LOG_BAK="10"
 # 配置supervisor配置中的environment环境变量
-export SUP_APP_ENV="PRJ_ROOT=\\\"$PRJ_ROOT\\\",GIN_MODE=\\\"release\\\",LD_LIBRARY_PATH=\\\"$LD_LIBRARY_PATH\\\""
-# 设定sup [command] all 的遍历目录
-export SUP_BUILD_PATH=$build_all
-# -------------------------------------------------
+# export SUP_APP_ENV="PRJ_ROOT=\\\"$PRJ_ROOT\\\",GIN_MODE=\\\"release\\\",LD_LIBRARY_PATH=\\\"$LD_LIBRARY_PATH\\\""
+
 # 设定publish指令打包时需要包含的文件夹环境变量
+# -------------------------------------------------
 # 默认会打包以下目录：$PRJ_ROOT/bin/* $PRJ_ROOT/src/app/app等二进制程序
-export PUB_ROOT_RES="etc" # 根目录下需要打包的文件夹列表，如"etc"等
-export PUB_APP_RES="public" # app下的文件夹列表，如"res public"等
+# export PUB_ROOT_RES="etc" # 根目录下需要打包的文件夹列表，如"etc"等
+# export PUB_APP_RES="public" # app下的文件夹列表，如"res public"等
 
 # 更改路径可更改编译器的版本号, 如果未指定，使用系统默认的配置
 go_root="/usr/local/go"
