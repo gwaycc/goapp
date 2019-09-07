bd87af032581b84e35967f03e8d49e61ab0be3f0
shu <free1139@163.com>
2019-09-07 10:39:44 +0800

    fix env.sh

--- env.sh
+++ env.sh
@@ -2,53 +2,49 @@
 
 # Template from https://github.com/gwaycc/goapp
 
-# 项目需要导出的程序环境变量
+# Export environment for building or runing program
 # -------------------------------------------------
 export PRJ_ROOT=`pwd`
 export PRJ_NAME="goapp"
 export GOBIN=$PRJ_ROOT/bin
 export GO111MODULE=on
 
-# 设定sup [command] all 所遍历的目录
+# Setting directory of sup [command] all, split with space for multiply directory.
 export BUILD_ALL_PATH="$PRJ_ROOT/service/app $PRJ_ROOT/applet/web"
 
-# supervisord配置文件参数
+# Setting supd program params configuration
 ## --------------------START-------------------
-## 以下是部署时的supervisor默认配置数据，若未配置时，会使用以下默认数据
-## 开发IDE可不配置以下环境变量
-## 配置supervisor运行的用户，默认为当前用户
-#export SUP_USER=$USER
-## 配置supervisor的配置文件目录
-#export SUP_ETC_DIR="/etc/supervisor/conf.d/" # (可选)
-## 配置supervisor的子程序日志的单个文件最大大小
-#export SUP_LOG_SIZE="10MB"
-## 配置supervisor的子程序日志的最多文件个数
-#export SUP_LOG_BAK="10"
-## 配置supervisor配置中的environment环境变量
-#export SUP_APP_ENV="PRJ_ROOT=\\\"$PRJ_ROOT\\\",GIN_MODE=\\\"release\\\",LD_LIBRARY_PATH=\\\"$LD_LIBRARY_PATH\\\""
+export SUP_USER=$USER # default is $USER
+export SUP_ETC_DIR="/etc/supd/conf.d" # default is /etc/supd/conf.d
+export SUP_LOG_SIZE="10MB"
+export SUP_LOG_BAK="10"
+export SUP_APP_ENV="PRJ_ROOT=\\\"$PRJ_ROOT\\\",GIN_MODE=\\\"release\\\",LD_LIBRARY_PATH=\\\"$LD_LIBRARY_PATH\\\""
 
-# 设定publish指令打包时需要包含的文件夹环境变量
+# Seting collection to sup publish
 # -------------------------------------------------
-# 默认会打包以下目录：$PRJ_ROOT/bin/* $BUILD_ALL_PATH等二进制程序
-# export PUB_ROOT_RES="etc" # 根目录下需要打包的文件夹列表，如"etc"等, 空格字符串表示不使用
-# export PUB_APP_RES="public" # app下的文件夹列表，如"res public"等, 空格字符串表示不使用
+## Root directory to colect of project. default is etc, split with space for multiply directory.
+export PUB_ROOT_RES="etc" 
+## App directory to colect of application. default is public, split with space for multiply directory.
+export PUB_APP_RES="public" 
 
-# 更改路径可更改编译器的版本号, 如果未指定，使用系统默认的配置
+# Setting GOROOT
+# -------------------------------------------------
 go_root="/usr/local/go"
 if [ -d "$go_root" ]; then
     export GOROOT="$go_root"
 fi
 
-# 将GOBIN加入PATH
+# Setting GOBIN and PATH
+# -------------------------------------------------
 bin_path=$GOBIN:$GOROOT/bin:
-
 rep=${PATH/bin_path/""}
 if [ ! "$PATH" = "$rep" ]; then
-    PATH=$rep # 重新设定原值的位置
+    PATH=$rep 
 fi
 export PATH=$bin_path$PATH
 
-# 下载sup管理工具
+# Download sup to manage project
+# -------------------------------------------------
 if [ ! -f $GOBIN/sup ]; then
     type curl >/dev/null 2>&1||{ echo -e >&2 "curl not found, need install at first."; exit 0; }
     echo "Download sup to bin."
@@ -56,9 +52,6 @@ if [ ! -f $GOBIN/sup ]; then
     curl https://raw.githubusercontent.com/gwaycc/supd/v1/bin/sup -o $GOBIN/sup && \
     chmod +x $GOBIN/sup&&echo "Download sup done."|| exit 0
 fi
-
-# 设定git库地址转换, 以便解决私有库中https证书不可信的问题
-# git config --global url."git@git.gway.cc:".insteadOf "https://git.gway.cc"
 # --------------------END--------------------
 
 echo "Env have changed to \"$PRJ_NAME\""
